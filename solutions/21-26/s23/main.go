70107ce7d3922402f00bc062faaba74966795b57
Paul Nikolaevskiy <paulnikolaevsky@gmail.com>
2023-05-22 15:44:35 +0300

    Added a 23th task.

--- solutions/21-26/s23/main.go
+++ solutions/21-26/s23/main.go
@@ -1,3 +1,20 @@
 package main
 
+import "fmt"
+
 // Удалить i-ый элемент из слайса.
+func main() {
+	sl := []int{0, 0, 0, 1, 0, 0, 0}
+	fmt.Println(removeItem(sl, 3))
+}
+
+// pos - начинается от 0
+func removeItem(sl []int, pos int) []int {
+	r := make([]int, 0, len(sl)-1)
+
+	// Берем нужные участки
+	r = append(r, sl[:pos]...)
+	r = append(r, sl[pos+1:]...)
+
+	return r
+}
