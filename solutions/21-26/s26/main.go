3ca379c6ae29e898dfadc4af9761cf80bf2fc485
Paul Nikolaevskiy <paulnikolaevsky@gmail.com>
2023-05-24 18:33:49 +0300

    Added a 26th task.

--- solutions/21-26/s26/main.go
+++ solutions/21-26/s26/main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 // Разработать программу, которая проверяет, что все символы в строке
 // уникальные (true — если уникальные, false etc). Функция проверки должна быть
 // регистронезависимой.
+
+func main() {
+	sUnique := "ajnlg,m1234t"
+	sNotUnique := "daspfopIDHADFOHpsadfo"
+
+	fmt.Println(isUnique(sUnique))
+	fmt.Println(isUnique(sNotUnique))
+}
+
+func isUnique(s string) bool {
+	s = strings.ToLower(s)
+
+	var v void
+	counter := make(map[rune]void)
+
+	// Начинаем обход строки
+	for _, char := range s {
+		_, ok := counter[char]
+
+		// Если символ уже есть - заканчиваем выполнение
+		if ok {
+			return false
+		}
+		// Если нет, добавляем в счетчик
+		counter[char] = v
+	}
+	return true
+}
+
+type void interface{}
